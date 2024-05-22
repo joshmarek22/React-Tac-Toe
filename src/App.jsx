5c35f3d2f2f0c66c19228272e11d7c149310e570
joshm <josh.marek22@gmail.com>
2024-05-22 00:28:36 -0400

    tik tac toe

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,65 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect, useRef } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [isXTurn, setIsXTurn] = useState(true)
+  const [round, setRound] = useState(1)
+  const [board, setBoard] = useState([])
+  const container = useRef(null)
+  useEffect(() => {
+    setBoard([...container.current.children].map((child) => {
+      return child.innerHTML
+    }))
+    checkHorizontal()
+    console.log(board)
+  }, [])
+  
 
+  const boxClicked = (e) => {
+    e.stopPropagation()
+
+    if(!e.target.innerHTML){
+      if(isXTurn == true){
+        e.target.innerHTML = "X"
+      }else{
+        e.target.innerHTML = "O"
+      }
+      setIsXTurn(!isXTurn)
+      setRound(round + 1)
+      
+    }
+    
+  }
+  const test = (e) => {
+    console.log(e.children);
+  }
+
+const checkHorizontal = () => {
+  if(board[0] === board[1] && board[1] === board[2]){
+    console.log("win")
+  }
+}
+
+//Read wether box has X or O
+//check if it meets hori, vert, diag winning contidions 
+//return someting if win, null if not 
+
+  
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div ref={container}  id="game-container">
+        <div onClick={(e) => boxClicked(e)} className="game-box"></div>
+        <div onClick={(e) => boxClicked(e)} className="game-box"></div>
+        <div onClick={(e) => boxClicked(e)} className="game-box"></div>
+        <div onClick={(e) => boxClicked(e)} className="game-box"></div>
+        <div onClick={(e) => boxClicked(e)} className="game-box"></div>
+        <div onClick={(e) => boxClicked(e)} className="game-box"></div>
+        <div onClick={(e) => boxClicked(e)} className="game-box"></div>
+        <div onClick={(e) => boxClicked(e)} className="game-box"></div>
+        <div onClick={(e) => boxClicked(e)} className="game-box"></div>
+    </div>
+    
     </>
+
   )
 }
 
